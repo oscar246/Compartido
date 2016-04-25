374754d1ea11b6e05d025d2c11d798e1b72a45f9
Oscar Giraldo <oscardavid@jmspro.net>
2016-04-25 18:28:27 -0500

    Config compartido

--- app/src/main/java/com/snw/compartido/MainActivity.java
+++ app/src/main/java/com/snw/compartido/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private String oscar = "hola";
+    private String edgar = "edgar";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
